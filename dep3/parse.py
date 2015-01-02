2d94fdcb9fe968e6f138f7c6434597f415a412c0
Paul Tagliamonte <tag@pault.ag>
2015-01-01 21:20:45 -0500

    fix the parser

--- dep3/parse.py
+++ dep3/parse.py
@@ -23,7 +23,13 @@ def get_headers(fileobj):
             data[last] += line.strip() + "\n"
             continue
 
+        if ":" not in line:
+            break
+
         last, value = line.split(":", 1)
         value = value.strip()
         data[last] = value
+
+    if None in data:
+        data.pop(None)
     return data
